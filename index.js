dc603b50d507b568892aa5600f7bbda6798d77e4
Taha Bin Shafiq <muhammadtahaaa7@gmail.com>
2025-01-16 19:54:59 +0500

    All change

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 function citiesChecker () {
-var cities = ["Karachi", "Lahore", "Faisalabad", "Rawalpindi", "Multan",
-    "Hyderabad", "Gujranwala", "Peshawar", "Quetta",
-    "Islamabad", "Sialkot", "Bahawalpur", "Sukkur",
-    "Larkana", "Mingora", "Sahiwal", "Dera Ghazi Khan",
-    "Mirpur Khas", "Chiniot", "Okara"];
-
-    var userValue = document.getElementById('city').value;
-    if (userValue === cities[9]){
-        alert('Islamabad is the cleanest city of Pakistan!');
-    }else if(userValue === cities[0]){
-        alert('Karachi is the City of Light!')
-    }else{alert('This City is not Found')}
-
-}
-
-var userBtn = document.getElementById('btn');
-userBtn.addEventListener('click' , citiesChecker)
-
+    var cities = ["Karachi", "Lahore", "Faisalabad", "Rawalpindi", "Multan",
+        "Hyderabad", "Gujranwala", "Peshawar", "Quetta",
+        "Islamabad", "Sialkot", "Bahawalpur", "Sukkur",
+        "Larkana", "Mingora", "Sahiwal", "Dera Ghazi Khan",
+        "Mirpur Khas", "Chiniot", "Okara"];
+    
+    var citiesName = document.getElementById('city')
+    for(var i = 0; i <= cities.length; i++){
+        if(cities [i] === 'Islamabad'){
+         citiesName.innerHTML = '"Islamabad is the <br> cleanest city of <br> Pakitan!"'
+        }
+    }
+    }
+    var userBtn = document.getElementById('btn');
+    userBtn.addEventListener('click' , citiesChecker)
+    
